6235310e2c8159ab1a55d6bcbe78789b4f05144d
DavidBak <sejongbgy07@gmail.com>
2021-05-15 21:48:41 +0900

    S3 버킷

--- back/routes/post.js
+++ back/routes/post.js
@@ -5,20 +5,25 @@ const {isLoggedIn} = require('./middlewares')
 const multer = require('multer')
 const router = express.Router();
 const fs = require('fs')
+const multerS3 = require('multer-s3')
+const AWS = require('aws-sdk')
+
+
+AWS.config.update({
+  accessKeyId:process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey:process.env.S3_SECRET_ACCESS_KEY,
+  region: 'ap-northeast-2'
+})
 
 
 const upload = multer({
-    storage: multer.diskStorage({
-       destination(req,file,done){
-           done(null, 'uploads')
-       },
- 
-       filename(req, file, done){ // 제로초.png
-           const ext = path.extname(file.originalname) // 확장자 추출(.png)
-           const basename = path.basename(file.originalname, ext) // 제로초
-           done(null, basename  + '_' + new Date().getTime() + ext) // 제로초1234441.png
+    storage: multerS3({
+       s3: new AWS.S3(),
+       bucket: 'react-nodebird-practice-s3',
+       key(req,file,cb){
+         cb(null, `original/${Date.now()}_${path.basename(file.originalname)}`)
        }
-    }),
+       }),
     limits:{fileSize: 20*1024*1024}
  })
 
@@ -157,7 +162,7 @@ router.delete('/:postId',isLoggedIn,async(req,res,next)=>{
 
 router.post('/images',isLoggedIn, upload.array('image') ,(req, res, next)=>{
     console.log(req.files)
-    res.json(req.files.map((v)=>v.filename))
+    res.json(req.files.map((v)=>v.location))
 })
 
 router.get('/:postId', async (req, res, next) => { // GET /post/1
