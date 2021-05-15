862479e603a73e0ecddf3acc05e99fd90eaa8705
DavidBak <sejongbgy07@gmail.com>
2021-05-15 21:54:33 +0900

    backUrl 제거

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -16,7 +16,7 @@ const PostImages = ({ images }) => {
   if (images.length === 1) {
     return (
       <>
-        <img role="presentation" src={`${backUrl}/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
+        <img role="presentation" src={`${images[0].src}`} alt={images[0].src} onClick={onZoom} />
         {showImageZoom && <ImageZoom images={images} onClose={onClose} />}
       </>
     );
@@ -24,15 +24,15 @@ const PostImages = ({ images }) => {
   if (images.length === 2) {
     return (
       <>
-        <img role="presentation" style={{ width: '50%', display: 'inline-block' }} src={`${backUrl}/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
-        <img role="presentation" style={{ width: '50%', display: 'inline-block' }} src={`${backUrl}/${images[1].src}`} alt={images[1].src} onClick={onZoom} />
+        <img role="presentation" style={{ width: '50%', display: 'inline-block' }} src={`${images[0].src}`} alt={images[0].src} onClick={onZoom} />
+        <img role="presentation" style={{ width: '50%', display: 'inline-block' }} src={`${images[1].src}`} alt={images[1].src} onClick={onZoom} />
         {showImageZoom && <ImageZoom images={images} onClose={onClose} />}
       </>
     );
   }
   return (
     <>
-      <img role="presentation" style={{ width: '50%' }} src={`${backUrl}/${images[1].src}`} alt={images[0].src} onClick={onZoom} />
+      <img role="presentation" style={{ width: '50%' }} src={`${images[1].src}`} alt={images[0].src} onClick={onZoom} />
       <div
         role="presentation"
         style={{ display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign: 'middle' }}
